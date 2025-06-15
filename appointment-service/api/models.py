53b2b21fa5ed45f889adf7411aff443a59e8dfe7
minhlq2311 <minhlq2311@gmail.com>
2025-06-15 09:48:41 +0700

    Appointment service to mysql

--- appointment-service/api/models.py
+++ appointment-service/api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Appointment(models.Model):
-    patient_id = models.IntegerField()
-    doctor_id = models.IntegerField()
-    date = models.DateTimeField()
+ patient_id = models.IntegerField()
+ doctor_id = models.IntegerField()
+ date = models.DateTimeField()
--- appointment-service/appointment_service/settings.py
+++ appointment-service/appointment_service/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'appointment_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'appointment_db',
+        'USER': 'appointment_user',
+        'PASSWORD': '123456',
+        'HOST': 'mysql-appointment',  # Phải trùng với tên service trong docker-compose
+        'PORT': '3306',
     }
 }
 
